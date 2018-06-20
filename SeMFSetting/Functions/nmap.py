7343d8f2092470ba46ef8af1d25a83ca4a0d32c8
yu <gy071089@outlook.com>
2018-06-20 17:18:31 +0800

    更新批量扫描功能及使用说明

--- SeMFSetting/Functions/nmap.py
+++ SeMFSetting/Functions/nmap.py
@@ -19,7 +19,10 @@ def nmap_host_all(host):
     nm = nmap.PortScanner()
     nm.scan(host,'0-65535')
     if nm[host].state()== 'up':
-        return nm[host]['tcp']
+        try:
+            return nm[host]['tcp']
+        except:
+            return 0
     
 #获取指定网段内全部存活主机
 def nmap_alive_lists(segment):
